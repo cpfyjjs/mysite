4643cc7e28f43928c27b6433946b116302263c8c
cpfyjjs <cpfyjjs@foxmail.com>
2018-12-02 11:13:44 +0800

    01

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from blog import models
+
+admin.site.register(models.User)
+admin.site.register(models.Author)
+admin.site.register(models.Blog)
+admin.site.register(models.Article)
+admin.site.register(models.ArticlesDetail)
+
