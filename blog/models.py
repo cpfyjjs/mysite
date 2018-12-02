f706751eb9b22958899b38fda16c11308c3ad56f
cpfyjjs <cpfyjjs@foxmail.com>
2018-12-02 11:27:28 +0800

    02

--- blog/models.py
+++ blog/models.py
@@ -6,18 +6,40 @@ class User(models.Model):
     name = models.CharField(max_length=32)
     pwd = models.CharField(max_length=32)
 
+    def __str__(self):
+        return self.name
+
+
+
 class Author(models.Model):
     nickname = models.CharField(max_length=32)
     avatar = models.CharField(max_length=128)
 
     user = models.OneToOneField(to="User",on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.nickname
+
+
+
+
 class Blog(models.Model):
     title = models.CharField(max_length=32)
 
 
+    def __str__(self):
+        return self.title
+
+
 class Article(models.Model):
+    title = models.CharField(max_length = 32 )
     user = models.ForeignKey(to="User",on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.title
+
+
 
 
 
